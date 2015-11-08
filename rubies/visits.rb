048b735ef9176addfc20274b60d9b98c959b9663
Tim Zuev <tim.zuev@gmail.com>
2015-11-08 03:43:09 +0300

    made logic for showing and updating visits. now table looks as i want

--- rubies/visits.rb
+++ rubies/visits.rb
@@ -10,6 +10,7 @@ end
 get '/visits/:id/' do
   @visits = Visit.all
   @edit_id = params[:id].to_i
+  @time_end = @visits.get(@edit_id).time_end ? @visits.get(@edit_id).time_end : DateTime.now.round_time
   slim :visits
 end
 
@@ -20,18 +21,29 @@ delete '/visits/:id/' do
 end
 
 post '/visits/new/' do
-  params[:visit].each { |k,v| params[:visit][k] = v.to_i }
-  visit = Visit.new params[:visit]
+  vp = params[:visit]
+  vp.each { |k,v| vp[k] = v.to_i unless k == 'comment' }
+  visit = Visit.new
   visit.time_created = DateTime.now
-  visit.time_start = DateTime.now
+  visit.time_start = DateTime.parse("#{vp[:start_hour]}:#{vp[:start_minute]} +3")
+  visit.male = vp[:male]
+  visit.female = vp[:female]
+  visit.comment = vp[:comment]
   visit.save
   redirect to('/visits/')
 end
 
 put '/visits/:id/' do
+  vp = params[:visit]
+  vp.each { |k,v| vp[k] = v.to_i unless k == 'comment' }
   visit = Visit.get(params[:id])
-  visit.time_end = DateTime.now
-  visit.attributes = params[:visit]
+  visit.time_start = DateTime.parse("#{vp[:start_hour]}:#{vp[:start_minute]} +3")
+  visit.time_end = DateTime.parse("#{vp[:end_hour]}:#{vp[:end_minute]} +3")
+  visit.revenue = vp[:revenue]
+  visit.tips = vp[:tips]
+  visit.male = vp[:male]
+  visit.female = vp[:female]
+  visit.comment = vp[:comment]
   visit.save
   redirect to('/visits/')
 end

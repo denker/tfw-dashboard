d146f914f1c36e1550d41934fe4dfaff1918c24d
Tim Zuev <tim.zuev@gmail.com>
2016-02-07 16:44:15 +0300

    hide history functionality

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ set :partial_template_engine, :slim
 require './rubies/helpers'
 require './rubies/models' # file with require DataMapper and decriptions of all models
 require './rubies/visits' # contorollers for Visit model and /visits/ views
-require './rubies/history'
+#require './rubies/history'
 #require './rubies/stats'
 
 get %r{(/.*[^\/])$} do
@@ -21,9 +21,9 @@ get '/reports/' do
   #slim :reports
 end
 
-# get '/history/*' do
-  # redirect to '/visits/'
-# end
+get '/history/*' do
+  redirect to '/visits/'
+end
 
 get '/stats/' do
   redirect to '/visits/'

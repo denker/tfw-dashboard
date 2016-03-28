5f8e27478d63f46e90aff72ccfc8b5efba5306f3
Tim Zuev <tim.zuev@gmail.com>
2016-03-29 01:08:50 +0300

    add reports for 30 days/weeks/months, changed Line chart to Bar chart

--- rubies/reports.rb
+++ rubies/reports.rb
@@ -1,29 +1,45 @@
 get '/reports/' do
   # charts are requested in script.js
+  redirect to "/reports/day/"
+end
+
+get '/reports/:range/' do
+  redirect to "/reports/day/" unless params[:range].fix == params[:range]
+  @range = params[:range].fix # could be used for AJAX chart load
   slim :reports
 end
 
 
-get '/data.json/' do
+get '/data.json/:range' do
   # data for charts
-  sd = DateTime.get_work_date - 31
   hash = {
     labels: [],
-    datasets: [ {
-        fillColor: "rgba(210, 210, 210, 0.2)",
+    datasets: [
+      {
+        fillColor: "rgba(235, 182, 127, 0.53)",
         strokeColor: "rgba(185, 185, 185, 1)",
         pointColor: "rgba(199, 169, 149, 1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
         pointHighlightStroke: "rgba(205, 162, 151, 1)",
         data: []
-      } ]
-    }
+      }
+    ]
+  }
+  rng = params[:range].fix
+  finish = DateTime.get_work_date.rewind_to_work_date(rng)
+  labels = []
+  data   = []
+
   30.times do |i|
-    sd += 1
-    hash[:labels] << sd.strftime('%d.%m.%Y')
-    r = Visit.all(:started_at.gt => sd, :started_at.lt => sd + 1).sum(:revenue)
-    hash[:datasets].first[:data] << r.to_i
+    start = finish.start_of_prev(rng)
+    labels << start.strftime('%d.%m.%Y') # TODO special labels for weeks, months and years
+    data << Visit.all(:started_at.gt => start, :started_at.lte => finish).sum(:revenue).to_i
+    finish = start.start_of_prev(rng)
   end
+
+  hash[:labels] = labels.reverse
+  hash[:datasets].first[:data] = data.reverse
   json hash
+
 end

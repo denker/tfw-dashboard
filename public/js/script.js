568573e18103ab84a2168741fbe575b56190a2a6
Tim Zuev <tim.zuev@gmail.com>
2016-03-30 21:26:04 +0300

    custom scale for reports view

--- public/js/script.js
+++ public/js/script.js
@@ -47,11 +47,34 @@ $('#date-separator-control-unchecked').click( function() {
   $('.nothing-found').css("display", "block");
 });
 
+function calcStepValue(n) {
+  var i = 0, m = n, diffs = [];
+  while (m >= 1) { m = m/10; i++; } // i = digits in n
+  m = m * 10; // we need m greater than one
+  const baseValue = ((m * 1.1 / 6) + (m * 1.2 / 9)) / 2; //magic multiplicators and dividors to make a base scale value
+  const scaleValues = [0.125, 0.25, 0.5, 1]; // array where we pick a scale value
+  scaleValues.forEach( function(e,i,array){ diffs.push(Math.abs(e-baseValue)) });
+  var closestValueIndex = diffs.findIndex(function(e,i,array){return e == Math.min.apply(Math, array)}); // which scaleValue item is most closest to base value?
+  return scaleValues[closestValueIndex] * Math.pow(10,i-1);
+}
+
 if ($('canvas#dayRevenueChart').length) {
   var arr = window.location.href.split('/');
   var range = arr[arr.length-2];
   $.getJSON( "/data.json/"+range, function(data) {
+
+    // calculate custom Y-axis scale
+    var max = Math.max.apply(Math, data['datasets'][0]['data']);
+    var stepValue = calcStepValue(max);
+    var stepsNumber = 0, topPlanc = 0;
+    while (topPlanc < max) { topPlanc += stepValue; stepsNumber += 1; }
+
     var ctx = document.getElementById("dayRevenueChart").getContext("2d");
-    var myBarChart = new Chart(ctx).Bar(data, {scaleShowVerticalLines: false});
+    var myBarChart = new Chart(ctx).Bar(data, {
+      scaleShowVerticalLines: false,
+      scaleOverride: true,
+      scaleSteps: stepsNumber,
+      scaleStepWidth: stepValue,
+      scaleStartValue: 0});
   });
 }
